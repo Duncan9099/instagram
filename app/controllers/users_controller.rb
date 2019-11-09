16a0c0b2062a5cb575e746639580c281416f3d12
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-09 17:58:42 +0000

    users can be created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
   end
 
   def update
+  end
+
+  private
+
+  def user_params
+    {username: params[:username], email: params[:email], password: params[:password], profile_picture: params[:profile_picture]}
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,19 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "New user can be created" do
+    user = User.new(username: "User", email: "user@user.com", password_digest: "password", profile_picture: "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png")
+    assert user.save
+  end
+
+  test "User cannot signup without username" do
+    user = User.new(email: "user@user.com",password_digest: "password", profile_picture: "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png")
+    assert_not user.save, "username cannot be blank"
+  end
+
+  test "User cannot signup with invalid email" do
+    user = User.new(username: "User", email: "user",password_digest: "password", profile_picture: "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png")
+    assert_not user.save, "email is invalid"
+  end
+
 end
